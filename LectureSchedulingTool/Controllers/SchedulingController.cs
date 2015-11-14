a0c1d40c18fb7f4f7f68fccea82110d530b44d73
Илья Хованский <saskiuhia@gmail.com>
2015-11-14 21:44:25 +0200

    Refactor view's

--- LectureSchedulingTool/Controllers/SchedulingController.cs
+++ LectureSchedulingTool/Controllers/SchedulingController.cs
@@ -20,32 +20,40 @@ namespace LectureSchedulingTool.Controllers
             {
                 //Добавление нового элемента
                 case 'a':
-                ViewBag.action = 'a';
-                break;
+                    ViewBag.action = 'a';
+                    break;
                 //Сохранение нового элемента
                 case 's':
-                string name = Request.Form["name"];
-                string abbreviation = Request.Form["abbreviation"];
-                Faculty faculty = new Faculty(name, abbreviation);
-                DB.Faculty.Add(faculty);
-                DB.SaveChanges();
-                break;
+                    string name = Request.Form["name"];
+                    string abbreviation = Request.Form["abbreviation"];
+                    if (name.Length != 0 && abbreviation.Length != 0)
+                    {
+                        Faculty faculty = new Faculty(name, abbreviation);
+                        DB.Faculty.Add(faculty);
+                        DB.SaveChanges();
+                    }
+                    break;
                 //Редактирование существующего элемента
                 case 'e':
-                ViewBag.action = 'e';
-                ViewBag.row = row;
-                break;
+                    ViewBag.action = 'e';
+                    ViewBag.row = row;
+                    break;
                 //Обновление существующего элемента
                 case 'u':
-                DB.Faculty.Find(id_faculty).name = Request.Form["name"];
-                DB.Faculty.Find(id_faculty).abbreviation = Request.Form["abbreviation"];
-                DB.SaveChanges();
-                break;
+                    name = Request.Form["name"];
+                    abbreviation = Request.Form["abbreviation"];
+                    if (name.Length != 0 && abbreviation.Length != 0)
+                    {
+                        DB.Faculty.Find(id_faculty).name = name;
+                        DB.Faculty.Find(id_faculty).abbreviation = abbreviation;
+                        DB.SaveChanges();
+                    }
+                    break;
                 //Удаление существующего элемента
                 case 'r':
-                DB.Faculty.Remove(DB.Faculty.Find(id_faculty));
-                DB.SaveChanges();
-                break;
+                    DB.Faculty.Remove(DB.Faculty.Find(id_faculty));
+                    DB.SaveChanges();
+                    break;
             }
 
             //Передача списка факульетов в представление
@@ -64,36 +72,36 @@ namespace LectureSchedulingTool.Controllers
             {
                 //Добавление нового элемента
                 case 'a':
-                ViewBag.action = 'a';
-                break;
+                    ViewBag.action = 'a';
+                    break;
                 //Сохранение нового элемента
                 case 's':
-                string name = Request.Form["name"];
-                string abbreviation = Request.Form["abbreviation"];
-                int is_producing = Convert.ToInt32(Request.Form["is_producing"]);
-                int id_faculty = Convert.ToInt32(Request.Form["id_faculty"]);
-                Department department = new Department(name, abbreviation, is_producing, id_faculty);
-                DB.Department.Add(department);
-                DB.SaveChanges();
-                break;
+                    string name = Request.Form["name"];
+                    string abbreviation = Request.Form["abbreviation"];
+                    int is_producing = Convert.ToInt32(Request.Form["is_producing"]);
+                    int id_faculty = Convert.ToInt32(Request.Form["id_faculty"]);
+                    Department department = new Department(name, abbreviation, is_producing, id_faculty);
+                    DB.Department.Add(department);
+                    DB.SaveChanges();
+                    break;
                 //Редактирование существующего элемента
                 case 'e':
-                ViewBag.action = 'e';
-                ViewBag.row = row;
-                break;
+                    ViewBag.action = 'e';
+                    ViewBag.row = row;
+                    break;
                 //Обновление существующего элемента
                 case 'u':
-                DB.Department.Find(id_department).name = Request.Form["name"];
-                DB.Department.Find(id_department).abbreviation = Request.Form["abbreviation"];
-                DB.Department.Find(id_department).is_producing = Convert.ToInt32(Request.Form["is_producing"]);
-                DB.Department.Find(id_department).id_faculty = Convert.ToInt32(Request.Form["id_faculty"]);
-                DB.SaveChanges();
-                break;
+                    DB.Department.Find(id_department).name = Request.Form["name"];
+                    DB.Department.Find(id_department).abbreviation = Request.Form["abbreviation"];
+                    DB.Department.Find(id_department).is_producing = Convert.ToInt32(Request.Form["is_producing"]);
+                    DB.Department.Find(id_department).id_faculty = Convert.ToInt32(Request.Form["id_faculty"]);
+                    DB.SaveChanges();
+                    break;
                 //Удаление существующего элемента
                 case 'r':
-                DB.Department.Remove(DB.Department.Find(id_department));
-                DB.SaveChanges();
-                break;
+                    DB.Department.Remove(DB.Department.Find(id_department));
+                    DB.SaveChanges();
+                    break;
             }
 
             //Передача списков факультетов и кафедр в представление
@@ -113,34 +121,34 @@ namespace LectureSchedulingTool.Controllers
             {
                 //Добавление нового элемента
                 case 'a':
-                ViewBag.action = 'a';
-                break;
+                    ViewBag.action = 'a';
+                    break;
                 //Сохранение нового элемента
                 case 's':
-                string number = Request.Form["number"];
-                int people_capacity = Convert.ToInt32(Request.Form["peolple_capacity"]);
-                int id_department = Convert.ToInt32(Request.Form["id_department"]);
-                Classroom classroom = new Classroom(number, people_capacity, id_department);
-                DB.Classroom.Add(classroom);
-                DB.SaveChanges();
-                break;
+                    string number = Request.Form["number"];
+                    int people_capacity = Convert.ToInt32(Request.Form["peolple_capacity"]);
+                    int id_department = Convert.ToInt32(Request.Form["id_department"]);
+                    Classroom classroom = new Classroom(number, people_capacity, id_department);
+                    DB.Classroom.Add(classroom);
+                    DB.SaveChanges();
+                    break;
                 //Редактирование существующего элемента
                 case 'e':
-                ViewBag.action = 'e';
-                ViewBag.row = row;
-                break;
+                    ViewBag.action = 'e';
+                    ViewBag.row = row;
+                    break;
                 //Обновление существующего элемента
                 case 'u':
-                DB.Classroom.Find(id_classroom).number = Request.Form["number"];
-                DB.Classroom.Find(id_classroom).people_capacity = Convert.ToInt32(Request.Form["peolple_capacity"]);
-                DB.Classroom.Find(id_classroom).id_department = Convert.ToInt32(Request.Form["id_department"]);
-                DB.SaveChanges();
-                break;
+                    DB.Classroom.Find(id_classroom).number = Request.Form["number"];
+                    DB.Classroom.Find(id_classroom).people_capacity = Convert.ToInt32(Request.Form["peolple_capacity"]);
+                    DB.Classroom.Find(id_classroom).id_department = Convert.ToInt32(Request.Form["id_department"]);
+                    DB.SaveChanges();
+                    break;
                 //Удаление существующего элемента
                 case 'r':
-                DB.Classroom.Remove(DB.Classroom.Find(id_classroom));
-                DB.SaveChanges();
-                break;
+                    DB.Classroom.Remove(DB.Classroom.Find(id_classroom));
+                    DB.SaveChanges();
+                    break;
             }
 
             //Передача списков аудиторий и кафедр в представление
