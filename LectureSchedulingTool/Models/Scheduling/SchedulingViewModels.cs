6f90cc5e6420075cab3553267f68c7f5419e6178
Peter <shutka.intel@gmail.com>
2015-12-06 00:58:40 +0200

    new place

--- LectureSchedulingTool/Models/Scheduling/SchedulingViewModels.cs
+++ LectureSchedulingTool/Models/Scheduling/SchedulingViewModels.cs
@@ -7,20 +7,6 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace LectureSchedulingTool.Models
 {
-    public static class GetString
-    {
-        public static string GetContStr(this HtmlHelper helper, string name, string lang)
-        {
-            using (SchedulingContext data = new SchedulingContext())
-            {
-                var translation = data.Localization.FirstOrDefault(x => x.Name == name && x.Lang == lang);
-                if (translation != null) return translation.Text;
-            }
-            //А если такой строки нет, то надо об этом сообщить.
-            return "[No " + name + " for " + lang + "]";
-        }
-    }
-
     public class SVM
     {
         private static SchedulingContext DB = new SchedulingContext();
