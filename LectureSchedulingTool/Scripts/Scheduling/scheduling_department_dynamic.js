139190bd457bbe73b97f870730e41aeae1f12af9
Stas996 <sai199_@ukr.net>
2015-12-14 02:18:17 +0200

    улучшение корректности работы селект листов

--- LectureSchedulingTool/Scripts/Scheduling/scheduling_department_dynamic.js
+++ LectureSchedulingTool/Scripts/Scheduling/scheduling_department_dynamic.js
@@ -26,26 +26,28 @@
         success: function (data) {
             $('[name = ' + depSel + ']').empty();
             $('[name = ' + depSel + ']').append(data);
-            $('[name = ' + depSel + ']').removeAttr('disabled');
-            $('[class $= btn-primary]').filter('[value = u]').removeAttr('disabled');
-            $('[class $= btn-primary]').filter('[value = s]').removeAttr('disabled');
         }
     });
+
+    $('[name = ' + depSel + ']').removeAttr('disabled');
+    $('[class $= btn-primary]').filter('[value = u]').removeAttr('disabled');
+    $('[class $= btn-primary]').filter('[value = s]').removeAttr('disabled');
 }
 
 $(window).load(function () {
     
     $('[name = id_department]').change(function () {
         ChangeDepend('id_department', 'id_teacher');
+        
         ChangeDepend('id_department', 'id_subject');
     });
 
     $('[name = id_faculty]').change(function () {
         ChangeDepend('id_faculty', 'id_department');
-        $('[name = id_department]').change();
+        //$('[name = id_department] :first').change();
     });
 
-    $('[name = id_faculty]').ready(function () {
-        $('[name = id_faculty] :selected').change();
-    });
+    //$('[name = id_faculty]').ready(function () {
+    //    $('[name = id_faculty] :selected').change();
+    //});
 });
