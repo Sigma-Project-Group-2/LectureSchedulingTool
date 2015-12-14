6b3dd4beaaf634e674f3ed0cea7146bcf351a8a5
Stas996 <sai199_@ukr.net>
2015-12-14 15:31:06 +0200

    Улучшение работы селектов (рассмотрен случай с пустыми селектами)

--- LectureSchedulingTool/Scripts/Scheduling/scheduling_department_dynamic.js
+++ LectureSchedulingTool/Scripts/Scheduling/scheduling_department_dynamic.js
@@ -1,37 +1,42 @@
 ﻿function ChangeDepend(mainSel, depSel)
 {
     var id = $('[name = ' + mainSel + ']').val();
-    var partView;
-    switch(depSel)
-    {
-        case "id_department":
-            partView = "GetDepartments";
-            break;
-        case "id_teacher":
-            partView = "GetTeachers";
-            break;
-        case "id_subject":
-            partView = "GetSubjects";
-            break;
+    if (id == -1) {
+        $('[name = ' + depSel + ']').empty();
+        $('[name = ' + depSel + ']').append('<option value="-1">------</option>');
     }
-    $('[class $= btn-primary]').filter('[value = u]').prop('disabled', 'true');
-    $('[class $= btn-primary]').filter('[value = s]').prop('disabled', 'true');
-    $('[name = ' + depSel + ']').prop('disabled', 'true'); 
-     
-
-    $.ajax({
-        type: 'GET',
-        data: mainSel + "=" + id,
-        url: "/Scheduling/" + partView,
-        success: function (data) {
-            $('[name = ' + depSel + ']').empty();
-            $('[name = ' + depSel + ']').append(data);
+    else {
+        var partView;
+        switch (depSel) {
+            case "id_department":
+                partView = "GetDepartments";
+                break;
+            case "id_teacher":
+                partView = "GetTeachers";
+                break;
+            case "id_subject":
+                partView = "GetSubjects";
+                break;
         }
-    });
+        $('[class $= btn-primary]').filter('[value = u]').prop('disabled', 'true');
+        $('[class $= btn-primary]').filter('[value = s]').prop('disabled', 'true');
+        $('select').prop('disabled', 'true');
 
-    $('[name = ' + depSel + ']').removeAttr('disabled');
-    $('[class $= btn-primary]').filter('[value = u]').removeAttr('disabled');
-    $('[class $= btn-primary]').filter('[value = s]').removeAttr('disabled');
+
+        $.ajax({
+            type: 'GET',
+            data: mainSel + "=" + id,
+            url: "/Scheduling/" + partView,
+            success: function (data) {
+                $('[name = ' + depSel + ']').empty();
+                $('[name = ' + depSel + ']').append(data);
+            }
+        });
+
+        $('select').removeAttr('disabled');
+        $('[class $= btn-primary]').filter('[value = u]').removeAttr('disabled');
+        $('[class $= btn-primary]').filter('[value = s]').removeAttr('disabled');
+    }
 }
 
 $(window).load(function () {
@@ -44,7 +49,9 @@ $(window).load(function () {
 
     $('[name = id_faculty]').change(function () {
         ChangeDepend('id_faculty', 'id_department');
-        //$('[name = id_department] :first').change();
+        setTimeout(function () {
+            $('[name = id_department] :first').change();
+        }, 600);
     });
 
     //$('[name = id_faculty]').ready(function () {
